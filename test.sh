abd410d52be11292efdf6351f0315fc70630ff13
abhiram.venkatram@gmail.com <abhiram.venkatram@gmail.com>
2022-04-11 07:33:54 +0000

    trying the loop

--- test.sh
+++ test.sh
@@ -3,3 +3,21 @@
 echo -e "This is for searchhead"
 echo -e "This is a tryout"
 echo -e "Test shc deployment"
+
+counter=0
+
+MAX_RETRIES=600
+#TWT=Total Wait Time
+TWT=0
+
+ while [ ${TWT} -lt ${MAX_RETRIES} ];
+    do
+    sleep 1
+    echo "Running test loop"
+let TWT=${TWT}+1
+      if [ ${TWT} -ge ${MAX_RETRIES} ]; then
+        exitCode=1;
+        fail_logger "ERROR!!! Timedout"
+        exit $exitCode
+      fi
+  done
