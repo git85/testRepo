62cbd0ed7c2b687dc4b51aba2b11bf480f9b59f6
root <root@ip-10-0-12-52.ap-south-1.compute.internal>
2022-04-18 10:40:36 +0000

    testing git

--- poc/run.sh
+++ poc/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 /ansible/bin/ansible-playbook "/ansible/playbooks/playbook2.yml" -i "/ansible/playbooks/inventory.ini" -e "targethosts=linux" -e "ansible_ssh_user=ec2-user"
+echo "Finished"
