0809d00448f94c4af925c644ae047fa9d5fd0718
root <root@ip-10-0-12-52.ap-south-1.compute.internal>
2022-04-18 10:43:29 +0000

    removed duplicate

--- poc/poc/poc/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/ansible/bin/ansible-playbook "/ansible/playbooks/playbook2.yml" -i "/ansible/playbooks/inventory.ini" -e "targethosts=linux" -e "ansible_ssh_user=ec2-user"
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "starting git pull"
 git pull
-echo "Finished"
-cp -R poc /opt/concourse/
-echo "Building docker image"
+#echo "Finished"
+#cp -R poc /opt/concourse/
+#echo "Building docker image"
 
-cd /opt/concourse/poc/
-docker build -t abhiram85/pocdeploy .
-echo finished building 
+#cd /opt/concourse/poc/
+#docker build -t abhiram85/pocdeploy .
+#echo finished building 
 
-echo "pushing docker image"
-docker push abhiram85/pocdeploy:latest
-echo "finished docker push"
+#echo "pushing docker image"
+#docker push abhiram85/pocdeploy:latest
+#echo "finished docker push"
 
 
 
